a60b8d3556d09d1e44a284744bfe19029ebdd9c9
ida-t <idaliya.taisina@gmail.com>
2015-08-23 16:53:10 +0300

    5-8

---
+++ ida-express/6.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express()
+var stylus = require('stylus');
+var crypto = require('crypto')
+app.put('/message/:id', function(req, res){
+
+	res.end(crypto.createHash('sha1').update(new Date().toDateString() + req.params.id).digest('hex'));
+})
+
+app.listen(process.argv[2]);
